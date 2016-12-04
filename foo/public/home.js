48862c0a7b270c240fa87c04319ad2696572bad6
Michael <anthony_michael_wilson@yahoo.com>
2016-12-03 18:00:46 -0800

    update

--- foo/public/home.js
+++ foo/public/home.js
@@ -3,9 +3,47 @@
 
 	var timer;
 	var time = 0;
+	var timer2;
+	var jumpCount = 1;
+	var MAX_JUMPHEIGHT = 100;
+	var switchJump = false;
+	var jumping = false;
 
 	window.onload = function() {
-		animate();
+		animate();	
+		createPlayer();		
+	}
+
+	/*
+	 * Function that creates a player
+	 */
+	function createPlayer() {
+		var player = document.createElement("div");
+		player.id = "character";
+		player.style.left = 0 + "px";
+		player.style.top = window.innerHeight - 25 + "px";
+		document.getElementById("container").appendChild(player);
+		window.addEventListener("keypress", move);
+	}
+
+	/* 
+	 * Moves the character 
+	 *
+	 * @param takes in an event
+	 */
+	function move(e) {
+		var player = document.getElementById("character");
+		var x = parseInt(player.style.left);
+		var y = parseInt(player.style.top);		
+		if(e.keyCode == 37)	// left
+			player.style.left = x - 15 + "px";
+		else if(e.keyCode == 39) // right
+			player.style.left = x + 15 + "px";
+		else // space bar 
+			if(!jumping) {
+				jumping = !jumping;
+				animateJump(); 
+			}
 	}
 
 	function makeDrop() {
@@ -23,19 +61,64 @@
 	 * Function that animates blocks
 	 */
 	 function rain() {
-	 	if(time % 3 == 0) {
+	 	if(time % 2 == 0) {
 	 		makeDrop();
 	 	}	 	
 	 	time++;
 	 	var list = document.querySelectorAll(".blocks");
-	 	for(var i = 0; i < list.length; i++) {
-	 		list[i].style.top = parseInt(list[i].style.top) + 10 + "px";
+	 	var maxHeight = window.innerHeight
+			|| document.documentElement.clientHeight
+			|| document.body.clientHeight;
+	 	for(var i = 0; i < list.length; i++) {	 		
+	 		if(parseInt(list[i].style.top) > maxHeight - 25) 
+	 			list[i].parentNode.removeChild(list[i]);	 		
+	 		else 
+	 			list[i].style.top = parseInt(list[i].style.top) + 10 + "px";
 	 	}
 	 }
 
+	/*	  
+	 * function that animates a jump
+	 */
+	function animateJump() {
+		if(timer2 == null) {
+			timer2 = setInterval(jump, 20);			
+		} else {
+			clearInterval(timer);
+			timer2 = null;
+		}
+	}
+
+	/*
+	 * Deals with animating the jump of the character
+	 */
+	function jump() {
+		var player = document.getElementById("character");
+		
+		if(!switchJump && jumpCount < MAX_JUMPHEIGHT)
+		{
+			player.style.top = parseInt(player.style.top) - 2 + "px";
+			jumpCount++;
+			if(jumpCount == MAX_JUMPHEIGHT)				
+				switchJump = !switchJump;
+		} else {
+			player.style.top = parseInt(player.style.top) + 2 + "px";
+			jumpCount--;
+			if(jumpCount == 0) {
+				switchJump = !switchJump;
+				clearInterval(timer2);
+				timer2 = null;	
+				jumping =  !jumping;			
+			}
+
+		} 
+
+
+	}
+
 	 function animate() {
 	 	if(timer == null)
-	 		timer = setInterval(rain, 10);
+	 		timer = setInterval(rain, 50);
 	 	else {
 	 		clearInterval(timer);
 	 		timer = null;
---
+++ foo/public/home.php
@@ -0,0 +1,4 @@
+<?php 
+	$Name = $_GET["name"];
+	print json_encode($Name);
+?>
